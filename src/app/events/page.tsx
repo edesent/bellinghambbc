cc69314a46c0fe11442dd61048ce1bfee0623f45
pastor-gif <pastor@bellinghambbc.com>
2026-05-17 06:56:30 -0400

    Update events page to use Memorial Day Cookout1 image

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -34,7 +34,7 @@ export default function EventsPage() {
               </div>
               <div className="grid gap-6 md:grid-cols-2">
                 <Image
-                  src="/bbbc/Memorial Day Cookout.png"
+                  src="/bbbc/Memorial Day Cookout1.png"
                   alt="Memorial Day Cookout at Bellingham Bible Baptist Church"
                   width={900}
                   height={900}
